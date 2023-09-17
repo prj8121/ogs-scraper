88f9ef5cf14cf2b925fb8ff209fc3f98ff03fa6a
Parker Johnson <nationalparx@gmail.com>
2023-09-17 11:02:00 -0700

    Example text input (to be eventually used as a form for the username)

--- ogs-scraper-site/App.js
+++ ogs-scraper-site/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! spooky like magic wow flexible</Text>
+      <TextInputExample />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +20,28 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+const TextInputExample = () => {
+  const [inputText, setInputText] = useState(''); // Initialize a state variable to store the input text
+
+  const handleInputChange = (text) => {
+    setInputText(text); // Update the inputText state with the current text input
+  };
+
+  const handleSubmit = () => {
+    // Handle the submitted input text, e.g., you can log it or use it in some way
+    console.log('Input Text:', inputText);
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Type something..."
+        onChangeText={handleInputChange} // Call handleInputChange when the text input changes
+        value={inputText} // Set the value of the input to the inputText state
+      />
+      <Button title="Submit" onPress={handleSubmit} />
+    </View>
+  );
+};
