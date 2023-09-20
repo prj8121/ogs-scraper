babccbd88418f176f499877aceb4347b9bfd5e13
Parker Johnson <nationalparx@gmail.com>
2023-09-20 13:32:13 -0700

    Header from Navigation Stack Screen thing

--- ogs-scraper-site/App.js
+++ ogs-scraper-site/App.js
@@ -3,13 +3,52 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import React, { useState } from 'react';
 import TextInputExample from './components/TextInputExample';
 import styles from './styles/styles';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  return (
+    <AppNavigator/>
+  );
+}
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="S1"  
+          component={Screen1} 
+          options={{
+            headerStyle: {
+              backgroundColor: '#f2b844'
+            }
+          }}
+        />
+        <Stack.Screen name="S2" component={Screen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const Screen1 = () => {
   return (
     <View style={styles.container}>
       <Text style={{marginBottom:2}}>Input username</Text>
       <TextInputExample style={styles.textInput}/>
       <StatusBar style="auto" />
     </View>
-  );
+  )
+}
+
+const Screen2 = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={{marginBottom:2}}>Input username v2</Text>
+      <TextInputExample style={styles.textInput}/>
+      <StatusBar style="auto" />
+    </View>
+  )
 }
